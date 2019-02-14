bc3e53d0a9a2e31db86bf3dffdb1f5776aec9db2
Oleksandr Andrieiev <o.andrieiev@samsung.com>
2019-02-15 02:23:13 +0900

    refactor(ui): Generate background in one pass
    Add list of rects that contain node information to draw background
    pattern. Replaces recursion with explicit stack.

--- background.py
+++ background.py
@@ -35,52 +35,26 @@ class Background:
 def background_surface(width, height):
     surface = pygame.Surface((width, height))
     surface.fill((255, 255, 255))
-    BackgroundInternal(width, height).draw(surface, 0., 0.)
-    return surface
-
-
-class BackgroundInternal:
-    def __init__(self, x, y, depth=0):
-        self.x = x
-        self.y = y
-        self.depth = depth
-        self.color = random() * 100.
-        self.children = []
-        Node = collections.namedtuple("Node", "rect offset_x offset_y")
+    list = [(0., 0., width, height, 0)]
+    while list:
+        x1, y1, x2, y2, depth = list.pop()
+        print(x1, y1, x2, y2, depth)
         if random() < 0.5 and depth < 6:
-            offset = y*(random()*0.9 + 0.1)
-            self.children.append(Node(rect=BackgroundInternal(x, offset, depth+1),
-                                      offset_x=0, offset_y=0))
-            self.children.append(Node(rect=BackgroundInternal(x, y-offset, depth+1),
-                                      offset_x=0, offset_y=offset))
-        elif random() < 0.5 and depth < 6:
-            offset = x*(random()*0.9 + 0.1)
-            self.children.append(Node(rect=BackgroundInternal(offset, y, depth+1),
-                                      offset_x=0, offset_y=0))
-            self.children.append(Node(rect=BackgroundInternal(x-offset, y, depth+1),
-                                      offset_x=offset, offset_y=0))
-        elif random() < 1. and x > 20 and y > 20:
-            new_x_start = 10. + random()*90.
-            new_x_end = x - (10. + random()*90.)
-            new_y_start = 10. + random()*40.
-            new_y_end = y - (10. + random()*40.)
-            width = new_x_end - new_x_start
-            height = new_y_end - new_y_start
-            if width > 20. and height > 20.:
-                self.rect = (new_x_start, new_y_start, width, height)
+            y_ = y1 + (y2 - y1)*(random()*0.9 + 0.1)
+            list.append((x1, y1, x2, y_, depth + 1))
+            list.append((x1, y_, x2, y2, depth + 1))
+        elif random() < 0.0 and depth < 6:
+            x_ = x1 + (x2 - x1)*(random()*0.9 + 0.1)
+            list.append((x1, y1, x_, y2, depth + 1))
+            list.append((x_, y1, x2, y2, depth + 1))
+        else:
+            x1 += 10. + random()*90.
+            x2 -= 10. + random()*90.
+            y1 += 10. + random()*40.
+            y2 -= 10. + random()*40.
+            if x2 - x1 > 20. and y2 - y1 > 20.:
+                pygame.draw.rect(surface, (0, 0, 0), (x1, y1, x2 - x1, y2 - y1), 1)
                 if depth < 6:
-                    self.children.append(Node(rect=BackgroundInternal(width, height, depth + 1),
-                                              offset_x=new_x_start, offset_y=new_y_start))
+                    list.append((x1, y1, x2, y2, depth + 1))
 
-    def draw(self, surface, offset_x, offset_y):
-        try:
-            rect_to_draw = [self.rect[0] + offset_x,
-                            self.rect[1] + offset_y,
-                            self.rect[2],
-                            self.rect[3]]
-            pygame.draw.rect(surface, (0, 0, 0), rect_to_draw, 1)
-        except AttributeError:
-            pass
-        for child in self.children:
-            child.rect.draw(surface, child.offset_x + offset_x, child.offset_y + offset_y)
-
+    return surface
